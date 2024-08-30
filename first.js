59a67015617e1eb6a0ccbb0b18e34330725bd865
abhishek753395 <106075962+abhishek753395@users.noreply.github.com>
2024-08-30 13:25:07 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,14 @@
+// first.js
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // This function will run when the DOM is fully loaded
+    
+    // Example: Change the background color of the header on click
+    var header = document.querySelector('header');
+    header.addEventListener('click', function() {
+        header.style.backgroundColor = 'blue';
+    });
+    
+    // You can add more JavaScript code here to add interactivity to your website
+});
